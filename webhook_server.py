4e65ad53dd7566f285f79b0d9a49d910dfb2a762
cbrnt <3cbrnt@gmail.com>
2021-11-09 17:48:47 +0300

    KeyboardInterrupt and get value func

--- webhook_server.py
+++ webhook_server.py
@@ -14,43 +14,54 @@ HOST = '109.195.230.198'  # Standard loopback interface address (localhost)
 PORT = 8070
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-	sock.bind((HOST, PORT))
-	print('Binded port', PORT)
-	sock.listen(5) # limited to 5 connection in qeueue
-	while True:
-		conn, addr = sock.accept()
-		print('conn: ', conn)
-		print('Connected by', addr)
+def get_value(raw_list, parameter):
+	re_obj = re.compile(".*%s.*" % parameter)
+	value = list(filter(re_obj.match, raw_list))
+	print('%s: ' % parameter, value)
+	return value
 
+try:
 
-
-	#	with context.wrap_socket(sock, server_side=True) as ssock:
-	#		conn, addr = ssock.accept()
-	#		print('conn: ', conn)
-	#		print('Connected by', addr)
-
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+		sock.bind((HOST, PORT))
+		print('Binded port', PORT)
+		sock.listen(5) # limited to 5 connection in qeueue
 		while True:
-			data = conn.recv(1024)
-			print(type(data))
+			conn, addr = sock.accept()
+			print('conn: ', conn)
+			print('Connected by', addr)
 
-			if not data:
-				break
-			print('Data: ', data)
-			data = data.decode()
-			headers = data.split('\r\n', -1)
-			print(type(headers))
-			print('Headers: ', headers)
-			regexped_obj = re.compile(".*POST.*")
-			print('Regexp object: ', regexped_obj)
-			regexped_response = list(filter(regexped_obj.match, headers))
-			print('Regexped response: ')
-			needed_data = headers[len(headers)-1]
-			print('Needed data: ',needed_data)
-			get_values = needed_data.split('&', -1)
-			print('Got values: ', get_values)
-			re_obj = re.compile(".*channel_name.*")
-			channel_name = list(filter(re_obj.match, get_values))
-			print('Channnel name: ', channel_name)
-			
+
+
+		#	with context.wrap_socket(sock, server_side=True) as ssock:
+		#		conn, addr = ssock.accept()
+		#		print('conn: ', conn)
+		#		print('Connected by', addr)
+
+			while True:
+				data = conn.recv(1024)
+				print(type(data))
+
+				if not data:
+					break
+				print('Data: ', data)
+				data = data.decode()
+				headers = data.split('\r\n', -1)
+				print(type(headers))
+				print('Headers: ', headers)
+				regexped_obj = re.compile(".*POST.*")
+				print('Regexp object: ', regexped_obj)
+				regexped_response = list(filter(regexped_obj.match, headers))
+				print('Regexped response: ')
+				needed_data = headers[len(headers)-1]
+				print('Needed data: ',needed_data)
+				get_values = needed_data.split('&', -1)
+				print('Got values: ', get_values)
+				channel_name = get_values(needed_data, 'channel_name')
+				print('channel_name: ', channel_name)
+
+
+
+except KeyboardInterrupt:
+	sock.close()
 
