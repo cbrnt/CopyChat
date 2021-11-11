280ea3046a621e7e329b22a1c2dfced50157ec53
Вигерин Эдуард <vigerin@tochkak.ru>
2021-11-11 19:04:53 +0300

    Убрал break, если нет данных. Добавил переменную DEBUG

--- webhook_server.py
+++ webhook_server.py
@@ -15,6 +15,7 @@ import os
 HOST = '109.195.230.198'  # Standard loopback interface address (localhost)
 PORT = 8070
 
+DEBUG = 1
 
 
 while True:
@@ -23,11 +24,13 @@ while True:
 		# поднимаем TCP сокет
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.bind((HOST, PORT))
-		print('Binded port', PORT)
+		if DEBUG:
+			print('Binded port', PORT)
 		sock.listen(5)  # limited to 5 connection in queue
 		conn, addr = sock.accept()
-		print('conn: ', conn)
-		print('Connected by', addr)
+		if DEBUG:
+			print('conn: ', conn)
+			print('Connected by', addr)
 
 		#	with context.wrap_socket(sock, server_side=True) as ssock:
 		#		conn, addr = ssock.accept()
@@ -43,31 +46,36 @@ while True:
 			if data:
 				data = data.decode()
 				headers = data.split('\r\n', -1)
-				print('Headers: ', headers)
 				pattern = re.compile(".*token=.*")
 				result = [s for s in headers if pattern.search(s)]
 				if result:
-					print(result)
+					if DEBUG:
+						print(result)
 					attrib_list = re.split('&', result[0])
 					attrib_dict = dict()
 					for i in attrib_list:
 						splitted_att = re.split('=', i)
 						attrib_dict[splitted_att[0]] = splitted_att[1]
-					print(attrib_dict)
-					print(type(attrib_dict))
+					if DEBUG:
+						print(attrib_dict)
+						print(type(attrib_dict))
+
 					# Проверяю валидность токена из прилетевшего запроса
 					got_token = attrib_dict['token']
-					print('Token = ', got_token)
-					#true_token = 'npgVipKxFSz1iRdQoDorDuM6'
-					print('os.environ.values() = ',os.environ.values())
-					val = os.environ.values('TEST_OUT_WEBHOOK')
+					if DEBUG:
+						print('os.environ.values() = ',os.environ.values())
+					val = os.environ['TEST_OUT_WEBHOOK']
 					if val:
 						if val == got_token:
-							print('Env token = ', val, 'Got token', got_token)
+							if DEBUG:
+								print('Got valid token: %s', val)
 						else:
-							print('Token is not valid. Hacker?')
+							if DEBUG:
+								print('Token is not valid')
+
 				else:
-					print( 'So odd. Data received, headers in place, but Result is empty! Breaking loop iteration and going to next')
+					if DEBUG:
+						print( 'So odd. Data received, headers in place, but Result is empty! Breaking loop iteration and going to next')
 					break
 
 
